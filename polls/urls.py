faf556de94099ed162e5b43d7886a78a29b68ff7
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-08-24 00:45:07 +0700

    implement tutorial part 4

--- polls/urls.py
+++ polls/urls.py
@@ -4,14 +4,20 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # added the word 'specifics'
-    # path("specifics/<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/ -> question_id
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/ -> question_id results
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/ -> question_id vote
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+# urlpatterns = [
+#     # ex: /polls/
+#     path("", views.index, name="index"),
+#     # added the word 'specifics'
+#     # path("specifics/<int:question_id>/", views.detail, name="detail"),
+#     # ex: /polls/5/ -> question_id
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     # ex: /polls/5/results/ -> question_id results
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     # ex: /polls/5/vote/ -> question_id vote
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
