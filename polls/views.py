4d8c5da67847dc4018b0f069688df1f441e75866
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-09-08 17:40:40 +0700

    fix previous vote

--- polls/views.py
+++ polls/views.py
@@ -65,7 +65,11 @@ class DetailView(generic.DetailView):
             messages.error(request, "Question is closed.")
             return redirect("polls:index")
         # Render the page
-        return super().get(request, *args, **kwargs)
+        # return super().get(request, *args, **kwargs)
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        context["select_choice"] = select_choice
+        return self.render_to_response(context)
 
 
 class ResultsView(generic.DetailView):
