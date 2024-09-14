a44700bd2027d876076b60f9b15129f3fa25b7ed
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-09-14 22:17:06 +0700

    adjust field in admin site

--- polls/admin.py
+++ polls/admin.py
@@ -13,7 +13,16 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     """Class to customize the admin interface for the Question model."""
 
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information",
+         {"fields": ["pub_date", "end_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "end_date",
+                    "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
