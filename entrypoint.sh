51abeabb652ab5eb10749430d440d83c9103b20c
Chayakarn Hengsuwan <113860728+colarrbear@users.noreply.github.com>
2024-11-24 20:42:10 +0700

    Load initial data from fixtures

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 python ./manage.py migrate
+
+if [ -f "./data/polls-v4.json" ]; then
+  python ./manage.py loaddata ./data/polls-v4.json
+fi
+if [ -f "./data/users.json" ]; then
+  python ./manage.py loaddata ./data/users.json
+fi
+if [ -f "./data/votes-v4.json" ]; then
+  python ./manage.py loaddata ./data/votes-v4.json
+fi
+
 python ./manage.py runserver 0.0.0.0:8000
